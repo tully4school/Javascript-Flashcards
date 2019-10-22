848a143b13eea425b4bd1d64eb3410cda296557c
Joseph_Tully <tully4school@gmail.com>
2019-10-22 14:29:48 -0400

    trying to figure out logic

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 let htmlCard = document.querySelector(".card-container")
 let htmlQuestion = document.querySelector(".click .question")
 let htmlAnswer = document.querySelector(".click .answer")
+let next = document.querySelector(".next")
 
-class Card {
-	constructor(q,a){
-		this.question = q;
-		this.answer = a;
-	}
+
+//questions and answers
+let questions = {
+	"Attribute": "A key-value pair in an HTML element. You can use HTML attributes to control certain aspects of an element, like where the element links to or the size of the element.",
+	"Array": "A list of JavaScript values. In an array, each value has an index, which is the numbered position of that value in the array. The first value is at index 0, the next value is at index 1, and so on.",
 }
 
-let attribute = new Card("Attribute","A key-value pair in an HTML element. You can use HTML attributes to control certain aspects of an element, like where the element links to or the size of the element.")
-let array = new Card("Array", "A list of JavaScript values. In an array, each value has an index, which is the numbered position of that value in the array. The first value is at index 0, the next value is at index 1, and so on.")
+//dont know how im going to use this yet
+const question = Object.entries(questions)
+for (let i = 0; i < question.length; i++){
+	console.log(question[i][0])
+
+}
+
+//initial question
+htmlQuestion.textContent = question[0][0]
 
 
-htmlQuestion.textContent = attribute.question
+//logic for making the card change values
 htmlCard.addEventListener("click", function(evt){
 	evt.preventDefault()
 	htmlQuestion.classList.toggle("hidden")
 	htmlAnswer.classList.toggle("show")
-	htmlAnswer.textContent = attribute.answer
+	htmlAnswer.textContent = question[0][1]
 
+})
 
-// if (question.textContent = attribute.question){
-// 	(question.textContent = attribute.answer)
-
-// }
+next.addEventListener("click", function(evt){
+	evt.preventDefault()
+	console.log(evt)
+	htmlQuestion.textContent = question[1][0]
 })
